611652824700550cb7fa1ff9095f96f64c37e880
EberliAugusto <eberli.couto@gmail.com>
2023-03-19 14:40:23 +0100

    Removing unused code in icons assets

--- src/assets/Icons.js
+++ src/assets/Icons.js
@@ -3,22 +3,6 @@ import InfoIcon from '@mui/icons-material/Info';
 import PlaylistAddCheckIcon from '@mui/icons-material/PlaylistAddCheck';
 import JavaIcon from "./JavaIcon";
 
-
-
-export const refs = {
-  architecture: "architecture",
-  java: "java",
-  todo: "todo",
-  about: "about"
-}
-
-export const all = {
-  [refs.architecture]: ArchitectureIcon,
-  [refs.about]: InfoIcon,
-  [refs.todo]: PlaylistAddCheckIcon,
-  [refs.java]: JavaIcon,
-}
-
 export {
   ArchitectureIcon as architecture,
   JavaIcon as java,
